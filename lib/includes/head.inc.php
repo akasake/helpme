2b1a392cdf88fc333684da4525be7fa3b71cf45f
Akasake <pham_see@hotmail.com>
2018-05-02 12:47:13 +0200

    made a head file to include in heads for css links

---
+++ lib/includes/head.inc.php
@@ -0,0 +1,7 @@
+<?php if($_SERVER['HTTP_HOST'] == "localhost"): ?>
+    <base href="/helpme/">
+<?php else: ?>
+    <base href="/">
+<?php endif; ?>
+<link rel="stylesheet" href="css/normalize.min.css">
+<link rel="stylesheet" href="css/main.css">

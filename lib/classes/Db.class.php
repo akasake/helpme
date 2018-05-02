bde624b2fb28eac1fb9170a901012e14cfaf73e9
Akasake <pham_see@hotmail.com>
2018-05-02 13:17:53 +0200

    added Db class for connection with database

---
+++ lib/classes/Db.class.php
@@ -0,0 +1,28 @@
+<?php
+    class Db {
+
+        private static $conn;  
+        /**
+         * @return PDO connection
+         * -> if exists -> return existing
+         * -> if !exists -> return new PDO
+         * 
+         * Singleton: kijken of er een connectie is?
+         * Anders aanmaken!
+         */
+        public static function getInstance() {
+
+            if($_SERVER['HTTP_HOST'] == "localhost") {
+                include_once($_SERVER['DOCUMENT_ROOT'] ."/muse/lib/settings/db.php");
+            }else{
+                include_once($_SERVER['DOCUMENT_ROOT'] ."/muse/lib/settings/db.prod.php");
+            }
+            
+            if(self::$conn == null) {
+                self::$conn = new PDO("mysql:host=".$db["host"].";dbname=".$db["dbname"]."", $db["username"], $db["password"]);
+            }
+            return self::$conn;
+        }
+        
+    }
+?>

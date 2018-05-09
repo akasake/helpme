52474cf158922b49653d8146b97472487dbb0578
Akasake <pham_see@hotmail.com>
2018-05-09 11:42:46 +0200

    added a page only for first time users

---
+++ first_visit.php
@@ -0,0 +1,30 @@
+<?php
+    include_once("lib/settings/config.php");
+    $title = "Welcome";
+    
+
+    try {
+        
+    } 
+    catch(Exception $e){
+        
+    }
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php include("lib/includes/head.inc.php"); ?>
+    <title>Muse</title>
+</head>
+<body>
+    <?php include_once("lib/includes/nav.inc.php"); ?>  
+        
+    
+    
+    
+    <?php include_once("lib/includes/footer.inc.php"); ?>
+</body>
+</html> 

8e60c75ea822224ad9bbc020a99bf9b1440e0d3f
Akasake <pham_see@hotmail.com>
2018-05-09 15:19:26 +0200

    added getMelding function

--- lib/classes/Melding.class.php
+++ lib/classes/Melding.class.php
@@ -17,7 +17,19 @@
             // Checks if the values are set, else return null
             $statement->bindValue(":long", $long ?? null);
             $statement->bindValue(":lat", $lat ?? null);
+            // time is filled automatically in database
 			$statement->execute();
+
+        }
+        public static function getMeldingen(){
+            $conn = Db::getInstance();
+            $statement = $conn->prepare("
+                Select * from tl_melding 
+                limit 20 
+                order by tl_melding.time desc
+            ");
+            $statement->execute();
+            return $statement->fetchAll(PDO::FETCH_ASSOC);        
 
         }
 

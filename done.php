50c8a024961c79a149237dada5959a484ac24b19
Akasake <pham_see@hotmail.com>
2018-05-23 06:00:43 +0200

    feedback page and redirect to feedback

---
+++ done.php
@@ -0,0 +1,32 @@
+<?php
+    include_once("lib/settings/config.php");
+    $title = "HULP KOMT";
+    
+
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php include("lib/includes/head.inc.php"); ?>
+    <title><?php echo $title ?></title>
+    <style>
+      #map{
+        height: 600px;
+        width: 100%;
+       }
+    </style>
+</head>
+<body>
+<div class="canvas">
+    <?php include_once("lib/includes/nav.inc.php"); ?>       
+    <h1>Help is on the way!</h1>
+
+    <?php include_once("lib/includes/footer.inc.php"); ?>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCP6tWYDigepitMGesI3xXYxg2mnvVDPAs&callback=loadMaps" async defer></script>
+</div>
+</body>
+
+</html> 
